efe7421a7c56f9766a87cae1c77503b6336dd457
admin <admin@LAPTOP-ENVM1FNA>
2022-01-24 14:26:49 +0900

    My First Commit ^^

---
+++ HelloProject/src/main/java/com/example/world/Controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.world.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hi")
+	    public String first(HttpServletRequest request, Model model) throws Exception {
+
+			return "index";
+	    }
+
+}
